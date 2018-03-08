367d7336af826d8797fd0e7b263a502297bb0326
Brady Schnell <bradyschnell@gmail.com>
2018-03-07 21:17:39 -0800

    Added update script, debug start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-sudo docker run -d -p 44457:44457 --name database --net net teamrandint/database 
-sudo docker run -d -p 44455:44455 --name audit --net net teamrandint/auditserver 
-sudo docker run -d -p 44458:44458 --name trans --net net teamrandint/transactionserver
-sudo docker run -d -p 44456:44456 --name web --net net teamrandint/webserver
+sudo docker network create net
+sudo docker run -d -p --name database --net=net teamrandint/database 
+sudo docker run -d -p --name audit --net=net teamrandint/auditserver 
+sudo docker run -d -p --name trans --net=net teamrandint/transactionserver
+sudo docker run -d -p --name web --net=net teamrandint/webserver
